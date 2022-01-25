83637495c810f8ec9239a79c3f9a15a2c1ab864c
Shuakmakeasense <64322636+Bakytnur16@users.noreply.github.com>
2022-01-25 16:58:40 +0600

    Update exersice.py

--- exersice.py
+++ exersice.py
@@ -1,14 +1,14 @@
 # # 4.10.1
-# def Tostr(list):
-#     for i in range(0,len(list)):
-#         if list[i] == list[-1]:
-#             print('and',list[i])
-#         else:
-#             print(list[i],end=', ')
-#
-# spam = ['apples', 'bananas', 'tofu', 'cats','bear']
-# # print(Tostr(spam))
-# Tostr(spam)
+def Tostr(list):
+    for i in range(0,len(list)):
+        if list[i] == list[-1]:
+            print('and',list[i])
+        else:
+            print(list[i],end=', ')
+
+spam = ['apples', 'bananas', 'tofu', 'cats','bear']
+# print(Tostr(spam))
+Tostr(spam)
 
 #4.10.2
 
